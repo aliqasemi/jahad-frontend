be9ac3d38f382b1b04c0aaa2415a58309d3bc8b4
aliqasemi <qasemi4245qq@gmail.com>
2022-02-14 16:47:58 +0330

    edit in ui

--- src/components/Service/Item.vue
+++ src/components/Service/Item.vue
@@ -1,63 +1,75 @@
 <template>
-    <v-row class="item">
-        <v-col lg="2">
-            {{ index + 1 }}
-        </v-col>
-        <v-col lg="3">
-            {{ item.title }}
-        </v-col>
-        <v-col lg="2">
-            {{ item.user.firstname + ' ' + item.user.lastname }}
-        </v-col>
-        <v-col lg="5" style="text-align: left">
-            <router-link :to="{name:'AttachByService',  params: { service_id: item.id },}" style="text-decoration: none;">
-                <v-tooltip top>
-                    <template v-slot:activator="{ on ,attrs}">
-                        <v-btn
-                            slot="activator"
-                            v-bind="attrs"
-                            v-on="on"
-                        >
-                            <v-icon dark>fa-paperclip</v-icon>
-                        </v-btn>
+  <v-row class="item">
+    <v-col lg="1" style="font-weight: bolder">
+      {{ index + 1 }}
+    </v-col>
+    <v-col lg="4">
+      {{ item.title }}
+    </v-col>
+    <v-col lg="3">
+     <v-row style="background-color: #AED6D1; border-radius: 10px;text-align: center; height: fit-content; width: fit-content; margin: auto 0">
+       <v-col lg="2">
+         <v-icon>fa fa-user</v-icon>
+       </v-col>
+       <v-col lg="10">
+         {{ item.user.firstname + ' ' + item.user.lastname }}
+       </v-col>
+     </v-row>
+    </v-col>
+    <v-col lg="4" style="text-align: left">
+      <router-link :to="{name:'AttachByService',  params: { service_id: item.id },}"
+                   style="text-decoration: none; margin: 5px">
+        <v-tooltip top>
+          <template v-slot:activator="{ on ,attrs}">
+            <v-btn
+                style="background-color: lavenderblush"
+                slot="activator"
+                v-bind="attrs"
+                v-on="on"
+            >
+              <v-icon style="color: darkgreen" dark>fa-paperclip</v-icon>
+            </v-btn>
 
-                    </template>
-                    <span>نیازمندی های ارایه شده</span>
-                </v-tooltip>
-            </router-link>
-            <router-link :to="{name:'EditService',  params: { service_id: item.id },}" style="text-decoration: none;">
-                <v-tooltip top>
+          </template>
+          <span>نیازمندی های ارایه شده</span>
+        </v-tooltip>
+      </router-link>
+      <router-link :to="{name:'EditService',  params: { service_id: item.id },}"
+                   style="text-decoration: none;margin: 5px">
+        <v-tooltip top>
 
-                    <template v-slot:activator="{ on,attrs }">
+          <template v-slot:activator="{ on,attrs }">
 
-                        <v-btn
-                            slot="activator"
-                            v-bind="attrs"
-                            v-on="on"
-                        >
-                            <v-icon dark>fa-edit</v-icon>
-                        </v-btn>
+            <v-btn
+                 style="background-color: lavenderblush"
+                slot="activator"
+                v-bind="attrs"
+                v-on="on"
+            >
+              <v-icon style="color: darkcyan" dark>fa-edit</v-icon>
+            </v-btn>
 
-                    </template>
-                    <span>ویرایش</span>
-                </v-tooltip>
-            </router-link>
-            <v-tooltip top>
-                <template v-slot:activator="{ on , attrs}">
-                    <v-btn
-                        slot="activator"
-                        v-bind="attrs"
-                        v-on="on"
-                        @click.native="deleteDialog = true"
-                    >
-                        <v-icon dark>fa-trash</v-icon>
-                    </v-btn>
-                </template>
-                <span>حذف</span>
-            </v-tooltip>
-            <delete-modal v-model="deleteDialog" @action="deleteService(item.id)"/>
-        </v-col>
-    </v-row>
+          </template>
+          <span>ویرایش</span>
+        </v-tooltip>
+      </router-link>
+      <v-tooltip top style="margin: 5px">
+        <template v-slot:activator="{ on , attrs}">
+          <v-btn
+              style="background-color: lavenderblush"
+              slot="activator"
+              v-bind="attrs"
+              v-on="on"
+              @click.native="deleteDialog = true"
+          >
+            <v-icon style="color: red" dark>fa-trash</v-icon>
+          </v-btn>
+        </template>
+        <span>حذف</span>
+      </v-tooltip>
+      <delete-modal v-model="deleteDialog" @action="deleteService(item.id)"/>
+    </v-col>
+  </v-row>
 </template>
 
 <script>
@@ -65,42 +77,43 @@ import DeleteModal from "../GeneralComponent/deleteModal";
 import {mapActions} from "vuex";
 
 export default {
-    name: "Item",
-    props: {
-        item: {default: null},
-        index: {default: 0},
-    },
-    components: {
-        DeleteModal
-    },
-    data() {
-        return {
-            deleteDialog: false
-        }
-    },
-    methods: {
-        ...mapActions("service", ['removeService']),
-        async deleteService(id) {
-            let response;
-            response = await this.removeService(id);
-            if (!(response instanceof Error)) {
-                this.deleteDialog = false;
-            }
-        },
+  name: "Item",
+  props: {
+    item: {default: null},
+    index: {default: 0},
+  },
+  components: {
+    DeleteModal
+  },
+  data() {
+    return {
+      deleteDialog: false
     }
+  },
+  methods: {
+    ...mapActions("service", ['removeService']),
+    async deleteService(id) {
+      let response;
+      response = await this.removeService(id);
+      if (!(response instanceof Error)) {
+        this.deleteDialog = false;
+      }
+    },
+  }
 }
 </script>
 
 <style scoped>
 .item {
-    border: 2px inset darkseagreen;
-    border-radius: 5px;
-    margin: 5px;
+  border: 2px inset darkseagreen;
+  border-radius: 5px;
+  margin: 5px;
+  background-color: rgba(46,201,189,0.2);
 }
 
 .item:hover {
-    background-color: azure;
-    transition: 100ms;
-    border: 2px inset black;
+  background-color: rgba(202,200,5,0.4);
+  transition: 900ms;
+  border: 2px inset black;
 }
 </style>
--- src/views/Service/Services.vue
+++ src/views/Service/Services.vue
@@ -1,5 +1,5 @@
 <template>
-    <div style="width: 90%;margin: 0 auto; direction: rtl">
+    <div style="width: 90%;margin: 0 auto; direction: rtl;font-size: 20px;">
         <div style="direction: rtl; background-color: rgba(13,75,118,0.83);border-radius: 10px">
             <v-breadcrumbs :items="items">
                 <template v-slot:divider>
@@ -19,15 +19,21 @@
                 </template>
             </v-breadcrumbs>
         </div>
-        <v-row style="direction: rtl;margin-top: 20px">
-            لیست خدمت ها
-        </v-row>
-        <br>
-        <hr style="display: block; width: 75%"/>
-        <items/>
+        <div style="background-color: whitesmoke; padding: 20px; margin: 10px; border-radius: 10px">
+          <v-row style="direction: rtl;margin-top: 10px;color:  rgba(13,75,118,0.83);  letter-spacing: 3px;">
+            مدیریت خدمات
+          </v-row>
+          <br>
+          <div class="title-en">
+            <div style="position: absolute;text-align: left; background-color: rgba(13,75,118,0.83); color: #eeeeee; border-radius: 10px;padding: 7px;">Services</div>
+          </div>
+          <hr style="display: block; width: 100%"/>
+          <br>
+          <items/>
+        </div>
 
-        <router-link :to="{name:'AddService'}" style="text-decoration: none">
-            <v-btn>اضافه کردن خدمت جدید</v-btn>
+        <router-link :to="{name:'AddService'}" style="text-decoration: none; padding: 20px;">
+            <v-btn style=" font-weight: bolder; font-size: 15px; letter-spacing: 3px;background-color: rgba(13,75,118,0.83);color: white">اضافه کردن خدمت جدید</v-btn>
         </router-link>
     </div>
 </template>
@@ -65,5 +71,14 @@ export default {
     color: white;
     border-radius: 10px;
     transition: 1s;
+}
+.title-en {
+  letter-spacing: 5px;
+  font-size: 10px;
+  white-space: nowrap;
+  text-transform: uppercase;
+  color:#55706D;
+  display: block;
+  padding-bottom: 15px;
 }
 </style>
