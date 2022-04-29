3153e1f9683047ef64747ed81ba8127b0e682474
aliqasemi <qasemi4245qq@gmail.com>
2022-04-29 15:25:55 +0430

    fix ui

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,83 +1,89 @@
 <template>
-  <div style="background-color: #55706D; height: 1200px">
+  <div style="background-color: #eeeeea; min-height: 1200px">
+    <v-subheader style="text-align: right;direction: rtl; background-color: #1F7087; color: #eeeeea">
+      <v-icon color="white" style="left: 5px">fa fa-cogs</v-icon>
+      سامانه جهاد سازندگی
+    </v-subheader>
     <transition name="loader-transition">
       <v-row>
+        <v-col xl="6" lg="6" md="6" sm="12" xs="12" style="padding-top: 20px;direction: rtl">
+          <v-img style="border-radius: 10px" height="600px" width="450px" :src="require('@/images/jahad.jpg')">
+          </v-img>
+        </v-col>
         <v-col xl="6" lg="6" md="6" sm="12" xs="12">
           <v-form v-if="show" @submit.prevent="loginRequest" style="padding-top: 20px">
             <v-container>
-              <v-row style="margin: 0 auto">
-                <v-col style="margin: 0 auto"
-                       cols="12"
-                       md="4"
-                >
-                  <v-text-field
-                      v-model="username"
-                      label="شماره تلفن"
-                      required
-                  ></v-text-field>
-                </v-col>
-              </v-row>
-
-              <v-row style="margin: 0 auto">
-                <v-col style="margin: 0 auto"
-                       cols="12"
-                       md="4"
-                >
-                  <v-text-field
-                      v-model="password"
-                      type="password"
-                      label="رمز عبور"
-                      required
-                  ></v-text-field>
-                </v-col>
-              </v-row>
-
-              <v-row style="text-align: center">
-                <v-col style="margin: 0 auto"
-                       cols="12"
-                       md="4"
-                >
-                  <v-btn type="submit" elevation="2" block>
-                    ورود
-                  </v-btn>
-                  <router-link :to="{name:'ForgotPassword'}" style="text-decoration: none">
-                    <v-btn elevation="2" style="margin-top: 20px">
-                      فراموشی رمز عبور
-                    </v-btn>
-                  </router-link>
-
-                </v-col>
-              </v-row>
-              <v-row style="text-align: center">
-                <v-col style="margin: 0 auto"
-                       cols="12"
-                       md="4"
-                >
-                  <router-link to="register" style="text-decoration: none">
-                    <v-btn type="submit" elevation="2" block>
-                      ثبت نام
-                    </v-btn>
-                  </router-link>
-                </v-col>
-              </v-row>
-              <v-row style="text-align: center">
-                <v-col style="margin: 0 auto"
-                       cols="12"
-                       md="4"
-                >
-                  <router-link to="/" style="text-decoration: none">
-                    <v-btn type="submit" elevation="2" block>
-                      صفحه اصلی
-                    </v-btn>
-                  </router-link>
+              <v-row>
+                <v-col xl="6" lg="6" style="margin: 0 auto; border: solid #b1b1b1; border-radius: 10px; padding: 30px; min-height: 590px">
+                  <v-row style="margin: 0 auto">
+                    <v-col style="margin: 0 auto"
+                           cols="10"
+                    >
+                      <v-text-field
+                          v-model="username"
+                          label="شماره تلفن"
+                          required
+                          reverse
+                      ></v-text-field>
+                    </v-col>
+                  </v-row>
+                  <v-row style="margin: 0 auto">
+                    <v-col style="margin: 0 auto"
+                           cols="10"
+                    >
+                      <v-text-field
+                          v-model="password"
+                          type="password"
+                          label="رمز عبور"
+                          required
+                          reverse
+                      ></v-text-field>
+                    </v-col>
+                  </v-row>
+                  <v-row style="text-align: center">
+                    <v-col style="margin: 0 auto"
+                           cols="8"
+                    >
+                      <v-btn type="submit" block elevation="10" color="success" style="color: #eeeeea">
+                        <v-icon style="margin-right: 10px">fa fa-sign-in</v-icon>
+                        ورود
+                      </v-btn>
+                      <router-link :to="{name:'ForgotPassword'}" style="text-decoration: none;">
+                        <v-btn style="margin-top: 20px;width: 100%;color: #eeeeea" color="red">
+                          <v-icon style="margin-right: 10px">fa fa-meh-o</v-icon>
+                          فراموشی رمز عبور
+                        </v-btn>
+                      </router-link>
+                    </v-col>
+                  </v-row>
+                  <v-row style="text-align: center">
+                    <v-col style="margin: 0 auto"
+                           cols="8"
+                    >
+                      <router-link to="register" style="text-decoration: none">
+                        <v-btn color="primary" style="color: #eeeeea" type="submit" elevation="5" block>
+                          <v-icon style="margin-right: 10px">fa-wpforms</v-icon>
+                          ثبت نام
+                        </v-btn>
+                      </router-link>
+                    </v-col>
+                  </v-row>
+                  <v-row style="text-align: center">
+                    <v-col style="margin: 0 auto"
+                           cols="8"
+                    >
+                      <router-link to="/" style="text-decoration: none">
+                        <v-btn color="blue-grey" style="color: #eeeeea" type="submit" elevation="2" block>
+                          <v-icon style="margin-right: 10px">fa fa-home</v-icon>
+                          صفحه اصلی
+                        </v-btn>
+                      </router-link>
+                    </v-col>
+                  </v-row>
                 </v-col>
               </v-row>
             </v-container>
           </v-form>
-        </v-col>
-        <v-col xl="6" lg="6" md="6" sm="12" xs="12" style="padding-top: 20px">
-          <v-img style="border-radius: 10px" height="600px" width="450px" :src="require('@/images/jahad.jpg')">
-          </v-img>
         </v-col>
       </v-row>
     </transition>
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,221 +1,248 @@
 <template>
-  <div style="background-color: #55706D; height: 1000px">
+  <div style="background-color: #eeeeea; min-height: 1000px">
+    <v-subheader style="text-align: right;direction: rtl; background-color: #1F7087; color: #eeeeea">
+      <v-icon color="white" style="left: 5px">fa fa-cogs</v-icon>
+      سامانه جهاد سازندگی
+    </v-subheader>
     <transition name="loader-transition">
-      <v-form v-if="show" @submit.prevent="registerRequest" style="padding-top: 20px">
-        <v-container class="register">
-          <v-stepper
-              v-model="iterative"
-              vertical
-              tag="span"
-              alt-labels
-              dark
-              non-linear
-              class="stepper"
-          >
-            <v-stepper-step
-                :complete="iterative > 1"
-                step="1"
-            >
-              اطلاعات هویتی
-            </v-stepper-step>
+      <v-row>
+        <v-col xl="6" lg="6" md="12" sm="12" xs="12" style="padding-top: 20px;direction: rtl">
+          <v-img style="border-radius: 10px" height="600px" width="450px" :src="require('@/images/jahad.jpg')">
+          </v-img>
+        </v-col>
+        <v-col xl="6" lg="6" md="12" sm="12" xs="12" style="padding-top: 20px;direction: rtl">
+          <v-form v-if="show" @submit.prevent="registerRequest">
+            <v-container class="register">
+              <v-stepper
+                  v-model="iterative"
+                  vertical
+                  tag="span"
+                  alt-labels
+                  dark
+                  non-linear
+                  class="stepper"
+              >
+                <v-stepper-step
+                    :complete="iterative > 1"
+                    step="1"
+                    color="red"
+                >
+                  اطلاعات هویتی
+                </v-stepper-step>
 
-            <v-stepper-content style="text-align: center;margin-left: 0" step="1">
-              <v-card
-                  style="background-color: #55706D;margin-bottom: 5px"
-              >
-                <v-form ref="step1" lazy-validation>
-                  <v-row style="margin: auto; text-align: right; direction: rtl;">
-                    <v-col style="margin: 0 auto"
-                           xl="6" lg="6" md="6" sm="12" xs="12"
-                    >
-                      <v-text-field
-                          v-model="form.firstname"
-                          label="نام"
-                          required
-                          reverse
-                          :counter="15"
-                          :rules="firstNameRules"
-                      ></v-text-field>
-                    </v-col>
-                    <v-col style="margin: 0 auto"
-                           xl="6" lg="6" md="6" sm="12" xs="12"
-                    >
-                      <v-text-field
-                          v-model="form.lastname"
-                          label="نام خانوادگی"
-                          required
-                          reverse
-                          :counter="20"
-                          :rules="lastNameRules"
-                      ></v-text-field>
-                    </v-col>
-                  </v-row>
-                </v-form>
-              </v-card>
-              <v-btn
-                  @click="goStep2"
-                  color="primary"
-              >
-                ثبت اطلاعات
-              </v-btn>
-            </v-stepper-content>
-
-            <v-stepper-step
-                :complete="iterative > 2"
-                step="2"
-            >
-              اطلاعات ارتباطی
-            </v-stepper-step>
-
-            <v-stepper-content style="text-align: center;margin-left: 0" step="2">
-              <v-card
-                  style="background-color: #55706D;margin-bottom: 5px"
-              >
-                <v-form ref="step2" lazy-validation>
-                  <v-row style="margin: 0 auto; text-align: right; direction: rtl">
-                    <v-col style="margin: 0 auto"
-                           xl="6" lg="6" md="6" sm="12" xs="12"
-                    >
-                      <v-text-field
-                          v-model="form.phoneNumber"
-                          label="شماره تلفن"
-                          required
-                          type="number"
-                          reverse
-                          :rules="phoneNumberRules"
-                      ></v-text-field>
-                    </v-col>
-                    <v-col style="margin: 0 auto"
-                           xl="6" lg="6" md="6" sm="12" xs="12"
-                    >
-                      <v-text-field
-                          v-model="form.email"
-                          label="ایمیل"
-                          required
-                          type="email"
-                          reverse
-                          :rules="emailRules"
-                      ></v-text-field>
-                    </v-col>
-                  </v-row>
-                </v-form>
-              </v-card>
-              <v-btn @click="iterative = 1" color="warning">
-                برگشت به مرحله قبل
-              </v-btn>
-              <br>
-              <br>
-              <v-btn
-                  @click="goStep3"
-                  color="primary"
-              >
-                ثبت اطلاعات
-              </v-btn>
-            </v-stepper-content>
-
-            <v-stepper-step
-                :complete="iterative > 3"
-                step="3"
-            >
-              اطلاعات امنیتی
-            </v-stepper-step>
-
-            <v-stepper-content style="text-align: center;margin-left:0;" step="3">
-              <v-card
-                  style="background-color: #55706D;margin-bottom: 5px"
-              >
-                <v-form ref="step3" lazy-validation>
-                  <v-row style="margin: auto; text-align: right; direction: rtl;">
-                    <v-col style="margin: 0 auto"
-                           xl="6" lg="6" md="6" sm="12" xs="12"
-                    >
-                      <v-text-field
-                          v-model="form.password"
-                          label="رمز عبور"
-                          required
-                          reverse
-                          :rules="passwordRules"
-                          :type="visiblePass ? 'text' : 'password'"
-                          :append-icon="visiblePass ? 'fa fa-eye-slash' : 'fa fa-eye'"
-                          @click:append="visiblePass = !visiblePass"
-                      ></v-text-field>
-                    </v-col>
-                    <v-col style="margin: 0 auto"
-                           xl="6" lg="6" md="6" sm="12" xs="12"
-                    >
-                      <v-text-field
-                          v-model="form.password_confirmation"
-                          label="تکرار رمز عبور"
-                          required
-                          reverse
-                          :rules="repeatPasswordRules"
-                          :type="visibleRepPass ? 'text' : 'password'"
-                          :append-icon="visibleRepPass ? 'fa fa-eye-slash' : 'fa fa-eye'"
-                          @click:append="visibleRepPass = !visibleRepPass"
-                      ></v-text-field>
-                    </v-col>
-                  </v-row>
-                </v-form>
-              </v-card>
-              <v-btn @click="iterative = 2" style="margin-top: 5px" color="warning">
-                برگشت به مرحله قبل
-              </v-btn>
-              <br>
-              <br>
-              <v-btn
-                  color="primary"
-                  type="submit"
-              >
-                تایید نهایی
-              </v-btn>
-            </v-stepper-content>
-
-            <v-stepper-step step="4">
-              کد تایید
-            </v-stepper-step>
-            <v-stepper-content style="text-align: center;margin-left:0;" step="4">
-              <v-card
-                  style="background-color: #55706D;margin: 5px"
-              >
-                <v-container class="register" style="width: 100%">
-                  <v-col style="margin: 0 auto"
-                         cols="12"
-                         md="4"
+                <v-stepper-content style="text-align: center;margin-left: 0" step="1">
+                  <v-card
+                      style="background-color: #55706D;margin-bottom: 5px"
                   >
-                    <v-form @submit.prevent="confirmRegisterRequest">
-                      <v-text-field
-                          v-model="confirmForm.confirm"
-                          label="کدتایید"
-                          required
-                          reverse
-                      ></v-text-field>
-                      <v-btn type="submit" elevation="2" block>
-                        تایید کد
-                      </v-btn>
-                      <br>
-                      کد تایید را در یافت نکرده اید؟
-                      <br>
-                      <v-btn @click="confirmRegisterRepeat">کلیک کنید</v-btn>
+                    <v-form ref="step1" lazy-validation>
+                      <v-row style="margin: auto; text-align: right; direction: rtl;">
+                        <v-col style="margin: 0 auto"
+                               xl="6" lg="6" md="6" sm="12" xs="12"
+                        >
+                          <v-text-field
+                              v-model="form.firstname"
+                              label="نام"
+                              required
+                              reverse
+                              :counter="15"
+                              :rules="firstNameRules"
+                          ></v-text-field>
+                        </v-col>
+                        <v-col style="margin: 0 auto"
+                               xl="6" lg="6" md="6" sm="12" xs="12"
+                        >
+                          <v-text-field
+                              v-model="form.lastname"
+                              label="نام خانوادگی"
+                              required
+                              reverse
+                              :counter="20"
+                              :rules="lastNameRules"
+                          ></v-text-field>
+                        </v-col>
+                      </v-row>
                     </v-form>
-                  </v-col>
-                </v-container>
-              </v-card>
-            </v-stepper-content>
-          </v-stepper>
+                  </v-card>
+                  <v-btn
+                      @click="goStep2"
+                      color="blue-grey"
+                  >
+                    ثبت اطلاعات
+                  </v-btn>
+                </v-stepper-content>
+
+                <v-stepper-step
+                    :complete="iterative > 2"
+                    step="2"
+                    color="red"
+                >
+                  اطلاعات ارتباطی
+                </v-stepper-step>
+
+                <v-stepper-content style="text-align: center;margin-left: 0" step="2">
+                  <v-card
+                      style="background-color: #55706D;margin-bottom: 5px"
+                  >
+                    <v-form ref="step2" lazy-validation>
+                      <v-row style="margin: 0 auto; text-align: right; direction: rtl">
+                        <v-col style="margin: 0 auto"
+                               xl="6" lg="6" md="6" sm="12" xs="12"
+                        >
+                          <v-text-field
+                              v-model="form.phoneNumber"
+                              label="شماره تلفن"
+                              required
+                              type="number"
+                              reverse
+                              :rules="phoneNumberRules"
+                          ></v-text-field>
+                        </v-col>
+                        <v-col style="margin: 0 auto"
+                               xl="6" lg="6" md="6" sm="12" xs="12"
+                        >
+                          <v-text-field
+                              v-model="form.email"
+                              label="ایمیل"
+                              required
+                              type="email"
+                              reverse
+                              :rules="emailRules"
+                          ></v-text-field>
+                        </v-col>
+                      </v-row>
+                    </v-form>
+                  </v-card>
+                  <v-btn @click="iterative = 1" color="warning">
+                    برگشت به مرحله قبل
+                  </v-btn>
+                  <br>
+                  <br>
+                  <v-btn
+                      @click="goStep3"
+                      color="blue-grey"
+                  >
+                    ثبت اطلاعات
+                  </v-btn>
+                </v-stepper-content>
+
+                <v-stepper-step
+                    :complete="iterative > 3"
+                    step="3"
+                    color="red"
+                >
+                  اطلاعات امنیتی
+                </v-stepper-step>
+
+                <v-stepper-content style="text-align: center;margin-left:0;" step="3">
+                  <v-card
+                      style="background-color: #55706D;margin-bottom: 5px"
+                  >
+                    <v-form ref="step3" lazy-validation>
+                      <v-row style="margin: auto; text-align: right; direction: rtl;">
+                        <v-col style="margin: 0 auto"
+                               xl="6" lg="6" md="6" sm="12" xs="12"
+                        >
+                          <v-text-field
+                              v-model="form.password"
+                              label="رمز عبور"
+                              required
+                              reverse
+                              :rules="passwordRules"
+                              :type="visiblePass ? 'text' : 'password'"
+                              :append-icon="visiblePass ? 'fa fa-eye-slash' : 'fa fa-eye'"
+                              @click:append="visiblePass = !visiblePass"
+                          ></v-text-field>
+                        </v-col>
+                        <v-col style="margin: 0 auto"
+                               xl="6" lg="6" md="6" sm="12" xs="12"
+                        >
+                          <v-text-field
+                              v-model="form.password_confirmation"
+                              label="تکرار رمز عبور"
+                              required
+                              reverse
+                              :rules="repeatPasswordRules"
+                              :type="visibleRepPass ? 'text' : 'password'"
+                              :append-icon="visibleRepPass ? 'fa fa-eye-slash' : 'fa fa-eye'"
+                              @click:append="visibleRepPass = !visibleRepPass"
+                          ></v-text-field>
+                        </v-col>
+                      </v-row>
+                    </v-form>
+                  </v-card>
+                  <v-btn @click="iterative = 2" style="margin-top: 5px" color="warning">
+                    برگشت به مرحله قبل
+                  </v-btn>
+                  <br>
+                  <br>
+                  <v-btn
+                      color="primary"
+                      type="submit"
+                  >
+                    تایید نهایی
+                  </v-btn>
+                </v-stepper-content>
+
+                <v-stepper-step step="4" color="red">
+                  کد تایید
+                </v-stepper-step>
+                <v-stepper-content style="text-align: center;margin-left:0;" step="4">
+                  <v-card
+                      style="background-color: #55706D;margin: 5px"
+                  >
+                    <v-container class="register" style="width: 100%">
+                      <v-col style="margin: 0 auto"
+                             cols="12"
+                             md="4"
+                      >
+                        <v-form @submit.prevent="confirmRegisterRequest">
+                          <v-text-field
+                              v-model="confirmForm.confirm"
+                              label="کدتایید"
+                              required
+                              reverse
+                          ></v-text-field>
+                          <v-btn type="submit" color="success" elevation="2" block>
+                            تایید کد
+                          </v-btn>
+                          <br>
+                          کد تایید را در یافت نکرده اید؟
+                          <br>
+                          <v-btn @click="confirmRegisterRepeat">کلیک کنید</v-btn>
+                        </v-form>
+                      </v-col>
+                    </v-container>
+                  </v-card>
+                </v-stepper-content>
+              </v-stepper>
+            </v-container>
+          </v-form>
           <v-row style="text-align: center">
             <v-col style="margin: 0 auto"
-                   cols="12"
-                   md="4"
+                   cols="8"
             >
+              <router-link :to="{name:'Login'}" style="text-decoration: none;">
+                <v-btn block elevation="10" color="success" style="color: #eeeeea;margin-top: 20px;">
+                  <v-icon style="margin-right: 10px">fa fa-sign-in</v-icon>
+                  ورود
+                </v-btn>
+              </router-link>
+              <router-link :to="{name:'ForgotPassword'}" style="text-decoration: none;">
+                <v-btn style="margin-top: 20px;width: 100%;color: #eeeeea" color="red">
+                  <v-icon style="margin-right: 10px">fa fa-meh-o</v-icon>
+                  فراموشی رمز عبور
+                </v-btn>
+              </router-link>
               <router-link to="/" style="text-decoration: none">
-                <v-btn type="submit" elevation="2" block>
-                  بازگشت به صفحه اصلی
+                <v-btn color="blue-grey" style="margin-top: 20px;width: 100%;color: #eeeeea" type="submit" elevation="2" block>
+                  <v-icon style="margin-right: 10px">fa fa-home</v-icon>
+                  صفحه اصلی
                 </v-btn>
               </router-link>
             </v-col>
           </v-row>
-        </v-container>
-      </v-form>
+        </v-col>
+      </v-row>
     </transition>
   </div>
 </template>
@@ -336,15 +363,17 @@ export default {
 }
 
 .register {
-  background: antiquewhite;
-  margin-top: 20px;
-  width: 50%;
-  background: linear-gradient(30deg, #b1b1b1, cadetblue);
-  border-radius: 20px;
+  width: 80%;
+  border: inset #b1b1b1;
+  border-radius: 10px;
   direction: rtl;
 }
 
 .stepper .v-stepper__content {
   border: 1px none !important;
+}
+
+.v-stepper__label {
+  color: #1F7087 !important;
 }
 </style>
